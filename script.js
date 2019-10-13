39073a1f57888a4204798bd80f7d65132636d8be
Suja <sujijothy@gmail.com>
2019-10-12 19:01:57 -0500

    sorted based on hrs in space

--- script.js
+++ script.js
@@ -4,32 +4,69 @@ function init() {
     // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
         response.json().then(function(json) {
-            console.log(json[0]);
+            //console.log(json[0]);
             
             let div=document.getElementById("container");
-            let mostTimeInSpace=0;
+            //let mostTimeInSpace=0;
               
-            json.sort(sortHrInSpace("hoursInSpace")); 
-            for(let i=0;i<json.length;i++)
+            //json.sort(sortHrInSpace("hoursInSpace")); 
+           /* data=json;
+            data.sort(function (a, b) {
+                return a.name.localeCompare(b.name);
+            });
+            document.write('<pre>' + JSON.stringify(data, 0, 4) + '</pre>');
+            */
+           data=json;
+           function GetSortOrder(prop) {  
+            return function(a, b) {  
+                if (a[prop] > b[prop]) {  
+                    return 1;  
+                } else if (a[prop] < b[prop]) {  
+                    return -1;  
+                }  
+                return 0;  
+            }  
+        }  
+          
+        data.sort(GetSortOrder("hoursInSpace")); 
+        let astronautCount=0;   
+         for(var item in data)
             {
-              div.innerHTML+=`
+                astronautCount+=1;
+                let activeValueGreen;
+                if (data[item].active === "true"){
+                    //var activeValue="Active:" + data[item].active;
+                    //console.log(activeValue);
+                  //var activeValueGreen= "Active: " + data[item].active.fontcolor("green");
+                   // console.log(activeValueGreen);
+                    var activeValue= "Active: " + data[item].active;
+                    activeValueGreen = activeValue.fontcolor("green");
+                    
+                }
+                else{
+                   activeValueGreen= "Active: " + data[item].active;
+                    //console.log(activeValueGreen);
+                }  
+                
+                div.innerHTML+=`
               <div id="container">
               
               
               <fieldset>
-               <h1>${json[i].firstName}</h1>
+               <h1>${data[item].firstName}</h1>
                <ul>
-                 <li><h3>Hours in space:${json[i].hoursInSpace}</h3></li>
-                 <li><h3>Active:${json[i].active}</h3></li>
-                 <li><h3>Skills:${json[i].skills}</h3></li>
-                 <img class="avatar" src=${json[i].picture}>
+                 <img class="avatar" src=${data[item].picture}>
+                 <li><h3>Hours in space:${data[item].hoursInSpace}</h3></li>
+                 <li><h3>${activeValueGreen}</h3></li>
+                 <li><h3>Skills:${data[item].skills}</h3></li>
+                
                 </ul>
                 
                 </fieldset>
             </div> 
             `
             }
-              
+            div.innerHTML+=`<h1>Total Number of Astronauts:${astronautCount}</h1>`
         });
   });
   }
